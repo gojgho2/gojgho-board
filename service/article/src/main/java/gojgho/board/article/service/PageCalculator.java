c86cb151cfab5a076073074efb2ef354feba6500
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2025-01-02 16:34:12 +0900

    first commit

---
+++ service/article/src/main/java/gojgho/board/article/service/PageCalculator.java
@@ -0,0 +1,9 @@
+package gojgho.board.article.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageCalculator {
+
+}
---
+++ service/hot-article/src/main/java/gojgho/board/hotarticle/HotArticleApplication.java
@@ -0,0 +1,12 @@
+package gojgho.board.hotarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotArticleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotArticleApplication.class, args);
+  }
+}
